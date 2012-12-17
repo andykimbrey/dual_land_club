4e33805ed1220e1ee41294a5f145736e1d3d336e
Andy Kimbrey <andrew@kimbrey.com>
2012-12-17 21:35:12 +0000

    Created unique Salt and Cipher keys

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'ficKn,Z5h)G1%Ek?RWArRw35>+m#W[=FGM1^bFtAb:Z;+');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '25203499869824556785293970067');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
