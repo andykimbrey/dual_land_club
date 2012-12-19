9303a9555741ea0f17e497d3386d76a9187f21d8
Andy Kimbrey <andrew@kimbrey.com>
2012-12-19 22:11:34 +0000

    Corrected validation error with the Land Model

--- app/Model/Land.php
+++ app/Model/Land.php
@@ -20,16 +20,6 @@ class Land extends AppModel {
  * @var array
  */
 	public $validate = array(
-		'id' => array(
-			'alphanumeric' => array(
-				'rule' => array('alphanumeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 		'Name' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
